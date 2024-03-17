c96fcb219243aa0fc616ac2c3da4075a0b69bd80
DaGeom Lee <dlekrja4523@gmail.com>
2024-03-17 16:39:20 +0900

    캡디

--- src/main/java/DataView/project/dto/CustomUserDetails.java
+++ src/main/java/DataView/project/dto/CustomUserDetails.java
@@ -1,12 +1,10 @@
 package DataView.project.dto;
 
 import DataView.project.domain.Member;
-import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
