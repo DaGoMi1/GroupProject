e9fba7759be37b7be115b629b1b6e38df543afca
yyeeng <tnghkrnldudnj123@gmail.com>
2024-03-22 11:48:35 +0900

    비밀번호 재확인 이거 이렇게 올리는거 맞나?

--- src/main/java/DataView/project/repository/SDJpaMemberRepository.java
+++ src/main/java/DataView/project/repository/SDJpaMemberRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SDJpaMemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByUserNumber(String userId);
-    Optional<Member> findByUserIdAndPassword(String userId, String password);
+    Optional<Member> findByUserNumber(String name);
+    Optional<Member> findByNameAndPassword(String name, String password);
     Optional<Member> findByEmail(String email);
 }
